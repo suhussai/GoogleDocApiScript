b488049c94c6d5ec319915e70b20a657dff6e590
suhussai <suhussai@ualberta.ca>
2015-08-11 22:46:09 -0600

    added function to check whether or not a given file is a googleDocFormat or not

--- writeToGoogleDoc.sh
+++ writeToGoogleDoc.sh
@@ -140,6 +140,25 @@ function renewAccessToken {
     echo "access_token = $access_token" >> $configFile
 }
 
+function checkType {
+    # if file has a downloadUrl 
+    # then the file type
+    # is not a googleDocFormat
+    #
+    # it is some uploaded file 
+    # with an extension like .txt
+    
+    fileID=$1
+    value=$(curl -X GET -H "Authorization: Bearer $access_token" https://www.googleapis.com/drive/v2/files/$fileID | grep downloadUrl)
+
+    if [ -z "$value" ]
+    then
+	googleDocFormat=true
+    else
+	googleDocFormat=false
+    
+}
+
 function checkToken {
     # checks if token is expired
     # updates variable expired
