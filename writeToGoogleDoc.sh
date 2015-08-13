f9ef9a1d4fe4b6bbeb91bc688ad2a5476e4d90b0
suhussai <suhussai@ualberta.ca>
2015-08-12 23:52:53 -0600

    - separated portion for handling option passing

--- writeToGoogleDoc.sh
+++ writeToGoogleDoc.sh
@@ -2,34 +2,7 @@
 
 
 
-OPTIND=1
-while getopts "h" OPTION
-do
-
-    case $OPTION in 
-	h)
-	    cat <<EOF
-
-Usage:
-
-./writeToGoogleDoc.sh sourceFile.txt destinationFileInGoogleDocs.txt
-
-arg1 = local file that will be the update for the file in google docs
-
-arg2 = the google docs file that will be updated with the contents of arg1
-
-EOF
-	    exit
-	    ;;
-	esac
-done
-
-# shift argument so that verbose options don't conflict with arguments 
-# for script 
-shift $((OPTIND-1))
-
-
-
+# get helper functions
 
 # file in google docs that will be updated 
 sourceFile=$1
