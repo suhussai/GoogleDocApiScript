5652b1c8bf5d7d7d41900f21040567acb53a6831
suhussai <suhussai@ualberta.ca>
2015-08-15 12:40:38 -0600

    moved opts back into writeToGoogleDoc script

--- writeToGoogleDoc.sh
+++ writeToGoogleDoc.sh
@@ -13,6 +13,36 @@ source helperFunctions.sh
 sourceFile=$1
 targetFile=$2
 
+
+
+OPTIND=1
+while getopts "h" OPTION
+do
+
+    case $OPTION in 
+	h)
+	    cat <<EOF
+
+Usage:
+
+./writeToGoogleDoc.sh sourceFile.txt destinationFileInGoogleDocs(.txt)
+
+arg1 = local file that will be the update for the file in google docs
+
+arg2 = the google docs file that will be updated with the contents of arg1
+
+EOF
+	    exit
+	    ;;
+	esac
+done
+
+# shift argument so that verbose options don't conflict with arguments 
+# for script 
+shift $((OPTIND-1))
+
+
+
 function readDocFile {
     # downloads google docs file for updating
     # argument is the title of 
