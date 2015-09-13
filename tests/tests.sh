194f897355b60c6707f1e9a8cdcbfa4ba57235a5
suhussai <suhussai@ualberta.ca>
2015-09-13 12:22:40 -0600

    added tests to tests.sh
    Added tests for several features, some of
    which are  not yet implemented.
    
    Basic tests exists for:
     - listing files
     - creating an empty file
     - writing to and reading from file
     - appending to file
     - deleting file

--- tests/tests.sh
+++ tests/tests.sh
@@ -11,7 +11,66 @@ cd ../tests/
     checkAndGetCredentialsIfNeeded
     checkAndRenewTokenIfNeeded
     listFiles > /tmp/tmpFile
-    # check /tmp/tmpFile
+    lines=$(cat /tmp/tmpFile | wc -l) 
+    [ "$lines" -ge 0 ]
+}
+
+@test "create empty test file in google docs" {
+    skip "Not yet implemented"
+    
+    listFiles > /tmp/tmpFile
+    count=$(grep -c "testFile" /tmp/tmpFile)
+    [ "$count" -eq 0 ]
+    
+    checkAndGetCredentialsIfNeeded
+    checkAndRenewTokenIfNeeded
+    creatFile "testFile"    
+    
+    listFiles > /tmp/tmpFile
+    count=$(grep -c "testFile" /tmp/tmpFile)
+    [ "$count" -eq 1 ]
+}
+
+@test "write and read test file" {
+    skip "Not yet implemented"
+
+    readFile "testFile" > /tmp/testFileContents
+    contents=$(cat /tmp/testFileContents)
+    [ "$contents" == "" ]    
+
+    writeToFile "test content" "testFile"
+
+    readFile "testFile" > /tmp/testFileContents    
+    contents=$(cat /tmp/testFileContents)
+    [ "$contents" == "test content" ]    
+
+}
+
+@test "append content to test file" {
+    skip "Not yet implemented"
+
+    appendToFile "test content on second line" "testFile"
+
+    readFile "testFile" > /tmp/testFileContents    
+    contents=$(cat /tmp/testFileContents)
+    [ "$contents" == "test content\ntest content on second line" ]    
+
+
+}
+
+@test "delete test file" {
+    skip "Not yet implemented"
+    
+    listFiles > /tmp/tmpFile
+    count=$(grep -c "testFile" /tmp/tmpFile)
+    [ "$count" -eq 1 ]
+
+    deleteFile "testFile"
+    
+    listFiles > /tmp/tmpFile
+    count=$(grep -c "testFile" /tmp/tmpFile)
+    [ "$count" -eq 0 ]
 
 }
  
+
