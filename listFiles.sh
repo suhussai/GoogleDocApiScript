eebcfdf6c496904a820123624a53a2175e56c9fe
suhussai <suhussai@ualberta.ca>
2015-09-12 20:58:25 -0600

    removed title and quotations from listfiles output

--- listFiles.sh
+++ listFiles.sh
@@ -28,7 +28,7 @@ function listDocFiles {
     cat tmpfiles2.txt | grep -B 1 -A 4 $searchTerm > tmpfiles3.txt
 
     # extract only titles and sort and filter out any duplicates
-    cat tmpfiles3.txt | grep title | sort -u > files.txt
+    cat tmpfiles3.txt | grep title | tr -d '"' |sed -e 's/title://g' | sort -u > files.txt
 
     cat files.txt
     rm files.txt tmpfiles.txt tmpfiles2.txt tmpfiles3.txt
